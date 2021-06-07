fac8bf444185d0be90a5d72263c27c65e8096df3
Nick Brown <bicknrown@gmail.com>
2021-06-07 16:35:47 -0400

    added deathlog, changed kick message to include death message

--- src/org/bicknrown/grouphardcore/DeathListener.java
+++ src/org/bicknrown/grouphardcore/DeathListener.java
@@ -7,30 +7,36 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import java.io.File;
+import java.io.FileWriter;
 
 public class DeathListener implements Listener {
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
 
-        System.out.println("[GroupHardcore] A Player Died!");
-        Bukkit.broadcastMessage(event.getEntity() + " Has Died!");
+        String deathMessage = event.getDeathMessage();
+        Bukkit.broadcastMessage(event.getEntity().getDisplayName() + " Has Died!");
         Bukkit.broadcastMessage("Server Resetting!");
 
         for(Player player : Bukkit.getOnlinePlayers()) {
             Bukkit.getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("GroupHardcore"), new Runnable() {
                 @Override
                 public void run() {
-                    player.kickPlayer("Server Resetting!");
+                    player.kickPlayer(deathMessage + ", Server Resetting!");
                 }
             });
         }
 
         File flag = new File("flag");
+        File deathLog = new File("deathlog.txt");
 
         try {
 
             flag.createNewFile();
+            deathLog.createNewFile();
+            FileWriter writer = new FileWriter("deathlog.txt",true);
+            writer.write(deathMessage + "\n");
+            writer.close();
             System.out.println("Flag Created");
 
         } catch(Exception e) {
